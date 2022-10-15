2cb192a95afb3382725bb2b0254361076ef541c2
frank-connolly <fconnolly1@gmail.com>
2022-10-15 12:40:09 +0200

    Initial commit - Spring boot skeleton

---
+++ src/test/java/com/frankconnolly/ironbank/IronBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankconnolly.ironbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
