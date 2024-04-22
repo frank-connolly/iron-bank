1846879a1db1ab11bde9278e785aadad6058917e
Frank Connolly <frank.connolly@cronn.de>
2024-04-22 08:26:30 +0200

    simplify project structure

--- src/main/java/com/frank/ironbank/repositories/WorkoutSetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.frank.ironbank.repositories;
-
-import com.frank.ironbank.models.WorkoutSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkoutSetRepository extends JpaRepository<WorkoutSet, Long> {
-}
